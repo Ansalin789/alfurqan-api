e322eb3602b935b7b158f4c99121643b6c5de87f
Ansalin7789 <ansalinsagar789@gmail.com>
2024-12-26 15:11:12 +0530

    Add payment details

--- src/api/payment/handler.ts
+++ src/api/payment/handler.ts
@@ -4,8 +4,8 @@ import Stripe from 'stripe';
 import { config } from "../../config/env";
 import EvaluationModel from "../../models/evaluation"
 import { Types } from "mongoose";
-
-
+import  PaymentDetailsModel  from "../../models/paymentDetails"
+import StudentPortModel from "../../models/alstudents";
 
 export const createPaymentIntent =  async (request: Request, h: ResponseToolkit) => {
   const { amount, currency, evaluationId } : any= request.payload;
@@ -16,24 +16,30 @@ export const createPaymentIntent =  async (request: Request, h: ResponseToolkit)
       _id: new Types.ObjectId(evaluationId)
     })
     console.log("evaluationDetails>>>",evaluationDetails);
-    const amount :any = evaluationDetails?.planTotalPrice
+   // const amount :any = evaluationDetails?.planTotalPrice
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency,
     });
-console.log()
-    // if(evaluationDetails){
-    //   evaluationDetails = {
-    //     studentStatus : "JOINED"
-    //   };
-    // }
-   
+    console.log("paymentIntent>>>",paymentIntent);
 
+   const savePaymentDetails = PaymentDetailsModel.create({
+    userId: evaluationDetails?._id,
+    userName: evaluationDetails?.student.studentFirstName,
+    paymentStatus: paymentIntent.status,
+    paymentAmount: paymentIntent.amount,
+    paymentResponse: paymentIntent,
+    paymentResponseId: paymentIntent.client_secret,
+    paymentDate: new Date(),
+    status: "Active",
+    createdBy: "System"
+   });
+   const paymentDetails =  (await savePaymentDetails).save();
     
-    if(evaluationDetails && evaluationDetails.studentStatus == "Joined" && evaluationDetails.classStatus == "Completed" ){
-     // await trialClassAssigned(evaluationDetails, teacherDetails)
+    if(paymentIntent.status == "succeeded" && evaluationDetails && evaluationDetails.studentStatus == "Joined" && evaluationDetails.classStatus == "Completed" ){
+      createStudentPortal(evaluationDetails);
     }
-   
+    console.log("paymentIntent>>>",paymentIntent);
 
     return h.response({
       clientSecret: paymentIntent.client_secret,
@@ -42,3 +48,37 @@ console.log()
     return h.response({ error: err }).code(400);
   }
 }
+
+
+ async function createStudentPortal(updatedEvaluation:any) {
+ 
+    const specialChars = '@#$%&*!';
+    const randomNum = Math.floor(Math.random() * 1000); // Random number between 0-999
+    const randomSpecial = specialChars[Math.floor(Math.random() * specialChars.length)]; // Random special character
+  
+    // Generate password
+    const firstThreeChars = updatedEvaluation.student.studentFirstName.substring(0, 3); // First 3 characters of the username
+    const reversedUsername = updatedEvaluation.student.studentFirstName.split('').reverse().join(''); // Reverse the username
+  
+    const password = `${firstThreeChars}${randomSpecial}${randomNum}${reversedUsername}`;
+
+  const createStudentPortal = await StudentPortModel.create({
+    student : {
+      studentId : updatedEvaluation.student.studentId,
+      studentEmail: updatedEvaluation.student.studentEmail,
+      studentPhone: updatedEvaluation.student.studentPhone
+    },
+    username: updatedEvaluation.student.studentFirstName,
+    password: password,
+    role: "Student",
+    status: "Active",
+    createdDate: new Date,
+    createdBy: updatedEvaluation.createdBy,
+    updatedDate: new Date
+  }
+   )
+
+const saveStudent = createStudentPortal.save()
+  return saveStudent;
+}
+  
