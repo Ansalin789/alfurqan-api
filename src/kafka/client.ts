47ebc8edae04131fc825bc0c30c6ce99daf7e5d5
rahul rm <rahulramachandiren@gmail.com>
2025-06-17 13:26:32 +0530

    old client

--- src/kafka/client.ts
+++ src/kafka/client.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 export const kafka = new Kafka({
   clientId: "erp-backend",
-    brokers: (process.env.KAFKA_BROKERS ?? "localhost:9092").split(","),
+ brokers: [process.env.KAFKA_BROKER ?? "kafka:9092"],
   retry: {
     initialRetryTime: 300,
     retries: 10,
