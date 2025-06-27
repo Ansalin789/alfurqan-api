3c28c8e5b0aae8b8a93bf25d401658c602cf8752
GomathiKanagaraj <gomathi2641999@gmail.com>
2025-06-27 12:51:42 +0530

    student list update

--- src/operations/classschedule.ts
+++ src/operations/classschedule.ts
@@ -65,7 +65,6 @@ export const updateStudentClassSchedule = async (
         _id: new Types.ObjectId(id)
       }).exec();
 
-      console.log("studentDetails>>>", studentDetails);
 
       // Fetch teacher details
       const teacherDetails = await UserModel.findOne({
@@ -73,7 +72,6 @@ export const updateStudentClassSchedule = async (
         userName: payload.teacher?.teacherName
       }).exec();
 
-      console.log("teacherDetails>>>", teacherDetails);
 
       // Map day name to numeric day (0=Sunday, 1=Monday, ..., 6=Saturday)
       const dayIndex = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].indexOf(day);
@@ -120,10 +118,8 @@ export const updateStudentClassSchedule = async (
         });
 
          const eventDetails = await createEvent(newClassSchedule);
-         console.log("eventDetails>>>", eventDetails);
 
         const savedClassSchedule = await newClassSchedule.save();
-        console.log("savedClassSchedule>>>>", savedClassSchedule);
         results.push(savedClassSchedule);
       }
     } catch (error) {
@@ -617,10 +613,6 @@ export const teacherStudentCount = async() =>{
 
 };
 
-
-
-
-
 export const teachingActivity = async (
   studentId: string
 ): Promise<{ month: string; completedHours: number; pendingHours: number; totalHours: number }[]> => {
@@ -792,7 +784,7 @@ export const updateteacherreschedule = async (
 
 
 export const getStudentClassCount  = async(studentId: string) =>{
-
+try{
   const studentClassCount= await ClassScheduleModel.aggregate([
     {
       $match: {
@@ -864,6 +856,10 @@ export const getStudentClassCount  = async(studentId: string) =>{
    const totalduration = (studentDurationCount[0].totalDuration).toFixed(2);
 
   return {totalClasses, totalAttendance, level, totalduration};
+
+}catch(e){
+  console.log("error: ", e);
+}
 };
 
 
@@ -1012,14 +1008,13 @@ export const getClassesWiseCount = async() => {
       { "teacher.teacherId": teacherId },
       { student: 1 }
     ).lean();
-
     const uniqueStudentsMap = new Map();
 
     for (const cls of classSchedules) {
       const student = cls.student;
       if (student?.studentId && !uniqueStudentsMap.has(student.studentId)) {
         const alstudent = await AlStudenModel.findOne({
-          "student.studentId": cls.student.studentId
+         _id: cls.student.studentId
         }).exec();
         let evaluation 
         if(alstudent){
