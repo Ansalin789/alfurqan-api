820b4bdcde5604df108f7bdec4f9a8cff4cecba6
rahul rm <rahulramachandiren@gmail.com>
2025-07-14 18:40:05 +0530

    new api level

--- src/server/plugins.ts
+++ src/server/plugins.ts
@@ -34,7 +34,7 @@ import salaryandwagesAPI from "../api/salarywages/router";
 import packageAPI from "../api/package/router"
 import leaveRequestAPI from "../api/leaveRequest/router";
 import TeachermeetingAPI from "../api/teacherMeeting/router"
-
+import Level from "../api/level/router"; 
 
 
 export const appPlugins = [
@@ -141,5 +141,8 @@ export const appPlugins = [
   },
   {
     plugin:TeachermeetingAPI,
+  },
+  {
+    plugin:Level,
   }
 ];
