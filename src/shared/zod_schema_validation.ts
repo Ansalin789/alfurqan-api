43e2f1d8cc6528f50b8dd7577fbe56342922cf39
GomathiKanagaraj <gomathi2641999@gmail.com>
2025-09-08 17:29:51 +0530

    Added student is

--- src/shared/zod_schema_validation.ts
+++ src/shared/zod_schema_validation.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const zodGetAllRecordsQuerySchema = z.object({
+  id: z.string().optional(),
   meetingId: z.string().optional(),
   courseId: z.string().optional(),
   roomId: z.string().optional(),
